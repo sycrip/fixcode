829c8239e47492fdd6ee8cb0378f5e54a0ca5924
firewater <firewater.cryyy@gmail.com>
2026-08-19 15:45:18 +0800

    Update edited code for test.cpp

--- code/1787123504329_modified.cpp
+++ code/1787123504329_modified.cpp
@@ -4,6 +4,9 @@ int main() {
     int a, b;
     for (int i = 1; i <= 10; i++)
         a = a + 1;
+        
+        
+        
     std::cin >> a >> b;
     std::cout << a + b << std::endl;
     return 0;
